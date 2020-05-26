c6fc531fb1e41e0d068d670b41c978a56b4d0b5f
Yao Josué <jyao2000@yahoo.fr>
2020-05-26 17:28:36 +0200

    Applications

---
+++ restaurant/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# M : Definir le schéma de nos données
+# V : Transmition des données et logique de l'application
+# T : Logique d'affichage
