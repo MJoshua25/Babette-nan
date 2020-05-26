233acd13889935199856779aa87c175834541674
Yao Josué <jyao2000@yahoo.fr>
2020-05-26 17:51:50 +0200

    home view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'pages/home-classic.html')
 
 # M : Definir le schéma de nos données
 # V : Transmition des données et logique de l'application
