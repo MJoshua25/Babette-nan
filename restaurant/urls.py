47ebfba6a97c823aade128f1e52a706fe50c2b4a
Yao Josué <jyao2000@yahoo.fr>
2020-05-26 18:37:43 +0200

    home view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,4 +5,8 @@ from restaurant import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu', views.menu, name='menu'),
+    path('gallery', views.gallery, name='gallery'),
+    path('faq', views.faq, name='faq'),
+    path('reservation', views.reservation, name='reservation'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -6,6 +6,20 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pages/home-classic.html')
 
+def menu(request):
+    return render(request, 'pages/menu-board.html')
+
+
+def gallery(request):
+    return render(request, 'pages/gallery-grid.html')
+
+
+def faq(request):
+    return render(request, 'pages/faqs.html')
+
+
+def reservation(request):
+    return render(request, 'pages/reservation.html')
 # M : Definir le schéma de nos données
 # V : Transmition des données et logique de l'application
 # T : Logique d'affichage
