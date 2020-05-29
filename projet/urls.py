7e43b487d6f14fd896d9a80f41696bae2c36faf4
Yao Josué <jyao2000@yahoo.fr>
2020-05-29 06:10:21 +0200

    url shop dans base.html

--- projet/urls.py
+++ projet/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
