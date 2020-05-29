9c5ed9b81ca6968aa45427e4ac916f4b05e11dfd
Yao Josué <jyao2000@yahoo.fr>
2020-05-29 06:04:43 +0200

    deplacement de fichiers

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/blog-carousel.html')
+    return render(request, 'pages/blog/blog-carousel.html')
 
 
 def single(request):
-    return render(request, 'pages/blog-single-post.html')
+    return render(request, 'pages/blog/blog-single-post.html')
