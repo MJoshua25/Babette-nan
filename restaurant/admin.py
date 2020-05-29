12facb24c33f104a2d73835df2f93f7b87efee34
Yao Josué <jyao2000@yahoo.fr>
2020-05-29 07:40:37 +0200

    admin menu fin tuto 3

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from restaurant import models
 
 # Register your models here.
+
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.MenuCategory, MenuCategoryAdmin)
