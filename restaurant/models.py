ee4e4f308473239fca64157a7d5a3e157b4416a2
Yao Josué <jyao2000@yahoo.fr>
2020-05-29 07:14:14 +0200

    fin tuto 2

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+# schemas -> relations -> base de donnée -> modéliser php -> requête -> afficher
+
+
+# schema -> modéliser -> views
+
