db3efd33d0175239212f684d8c3dfa41e726bf15
Yao Josué <jyao2000@yahoo.fr>
2020-06-04 05:55:48 +0200

    model ingredient

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,10 +13,27 @@ class MenuCategory(models.Model):
     titre = models.CharField(max_length=255)
 
     status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Catégorie de menu"
         verbose_name_plural = "Catégories de menu"
+
+    def __str__(self):
+        return str(self.titre)
+
+
+class Ingredient(models.Model):
+    titre = models.CharField(max_length=255)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Ingredient"
+        verbose_name_plural = "Ingredients"
 
     def __str__(self):
         return str(self.titre)
