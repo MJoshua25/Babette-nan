187d48f9a5e5364917fec43b3a808079d79cfe78
Yao Josué <jyao2000@yahoo.fr>
2020-06-04 06:28:08 +0200

    administration plats et ingredients

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -18,10 +18,81 @@ class MenuCategoryAdmin(admin.ModelAdmin):
         'titre',
     )
     list_per_page = 10
+    date_hierarchy = 'date_add'
     fieldsets = [
         ('Info', {
             'fields': [
                 'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+        'date_add',
+        'date_upd',
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+    date_hierarchy = 'date_add'
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+class PlatAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'prix',
+        'category',
+        'ingredients',
+        'isRecommended',
+        'status',
+        'date_add',
+        'date_upd',
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+    date_hierarchy = 'date_add'
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+                'prix',
+                'categorie',
+                'ingredients'
+            ]
+        }),
+        ('Recommendation', {
+            'fields': [
+                'isRecommended',
             ]
         }),
         ('Status et Activation', {
@@ -37,3 +108,5 @@ def _register(model, admin_class):
 
 
 _register(models.MenuCategory, MenuCategoryAdmin)
+_register(models.Ingredient, IngredientAdmin)
+_register(models.Plat, PlatAdmin)
